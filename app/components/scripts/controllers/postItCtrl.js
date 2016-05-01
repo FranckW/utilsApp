b497ff2590cba0af456ab7c91eeed06b2017891e
FranckW <franck.warlouzet@hotmail.fr>
2016-05-01 19:00:58 +0200

    remove cdn importation, all local now

--- app/components/scripts/controllers/postItCtrl.js
+++ app/components/scripts/controllers/postItCtrl.js
@@ -10,22 +10,28 @@ angular.module('utilsApp').controller('PostItCtrl', ["$scope", "$rootScope", "$h
 }]).directive('contenteditable', function ($timeout, $http) {
     return {
         restrict: "A",
-        scope: { ngModel: "="},
+        scope: { ngModel: "=" },
         link: function (scope, element, attr) {
             if (attr.id === "titleedit") {
-                   element.html(scope.ngModel.title);
-                }
-                else if (attr.id === "contentedit") {
-                   element.html(scope.ngModel.content);
-                }
+                element.html(scope.ngModel.title);
+            }
+            else if (attr.id === "contentedit") {
+                element.html(scope.ngModel.content);
+            }
             element.on('blur', function () {
+                var editedText = element.html();
+                var replacement = editedText.trim()
+                    .replace(/<br(\s*)\/*>/ig, '\n')
+                    .replace(/<[p|div]\s/ig, '\n$0')
+                    .replace(/(<([^>]+)>)/ig, "");
+
                 if (attr.id === "titleedit") {
-                    $http.get('http://localhost:8080/jeeServer/updatePostItTitle?id='+ scope.ngModel.id + '&title=' + element.text()).success(function (data) {
-                      });
+                    $http.get('http://localhost:8080/jeeServer/updatePostItTitle?id=' + scope.ngModel.id + '&title=' + editedText).success(function (data) {
+                    });
                 }
                 else if (attr.id === "contentedit") {
-                    $http.get('http://localhost:8080/jeeServer/updatePostItContent?id='+ scope.ngModel.id + '&content=' + element.text()).success(function (data) {
-                      });
+                    $http.get('http://localhost:8080/jeeServer/updatePostItContent?id=' + scope.ngModel.id + '&content=' + editedText).success(function (data) {
+                    });
                 }
                 scope.$apply();
                 return element;
