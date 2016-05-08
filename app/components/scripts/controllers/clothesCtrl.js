9b9a6eb323c93b6e44d3b25e23b0d6b482c23756
FranckW <franck.warlouzet@hotmail.fr>
2016-05-08 18:29:54 +0200

    started clothes app

---
+++ app/components/scripts/controllers/clothesCtrl.js
@@ -0,0 +1,6 @@
+'use strict';
+angular.module('utilsApp').controller('ClothesCtrl', function ($scope) {
+    
+
+
+});
--- app/components/scripts/controllers/postItCtrl.js
+++ app/components/scripts/controllers/postItCtrl.js
@@ -1,10 +1,5 @@
 'use strict';
-angular.module('utilsApp').controller('PostItCtrl', function ($scope, postItServices) {
-      $scope.oneAtATime = true;
-    
-    
-    
-    
+angular.module('utilsApp').controller('PostItCtrl', function ($document, $scope, postItServices) {
     function applyRemoteData(data) {
         $scope.postits = data.postits;
     }
@@ -29,6 +24,21 @@ angular.module('utilsApp').controller('PostItCtrl', function ($scope, postItServ
                 applyRemoteData(data);
             });
     };
+
+    $scope.toggleCollapse = function (id) {
+        var element = $document.find('#collapsePostItIcon_' + id);
+        var classList = $(element).attr("class").split(' ');
+        for (var i = 0; i < classList.length; i++) {
+            if (classList[i] === 'glyphicon-chevron-right') {
+                element.addClass('glyphicon-chevron-down');
+                element.removeClass('glyphicon-chevron-right');
+            }
+            else {
+                element.addClass('glyphicon-chevron-right');
+                element.removeClass('glyphicon-chevron-down');
+            }
+        }
+    }
 
     $scope.loadPostits();
 }); 
