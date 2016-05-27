4d3f9d2e626d3aeb84f9e77f098ff3ae384cd1d8
FranckW <franck.warlouzet@hotmail.fr>
2016-05-27 20:40:13 +0200

    started search by tag

---
+++ app/components/scripts/controllers/searchTagCtrl.js
@@ -0,0 +1,40 @@
+'use strict';
+angular.module('utilsApp').controller('SearchTagCtrl', function ($scope) {
+    $scope.searchTags = [{ id: "1", value: "" }, { id: "2", value: "" }, { id: "3", value: "" }];
+
+    $scope.addChoice = function () {
+        var index = $scope.searchTags.length + 1;
+        var dataObj = { id: index, value: '' };
+        $scope.searchTags.push(dataObj);
+    };
+
+    $scope.removeChoice = function (choice) {
+        $scope.choices.pop(choice);
+    };
+
+    $scope.pick = function () {
+        var randomIndex = 1 + Math.floor(Math.random() * $scope.choices.length);
+        $scope.result = $scope.choices[randomIndex - 1].value;
+        if (!$scope.result) {
+            $scope.result = "Entre des choix !";
+        }
+        $scope.open();
+    };
+
+    $scope.open = function () {
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: 'myModalContent.html',
+            controller: 'ClochetteModalInstanceCtrl',
+            resolve: {
+                items: function () {
+                    return true;
+                }
+            },
+            scope: $scope
+        });
+
+        modalInstance.result.then();
+    };
+
+});
